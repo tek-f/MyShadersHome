ff97fc4edc4a3d8598dfde704931cb0e11a1c6d6
tek-f <aaron.falco2@gmail.com>
2020-11-10 12:13:40 +1100

    Update

---
+++ Assets/Scripts/ActivateParticleSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateParticleSystem : MonoBehaviour
+{
+    [SerializeField] GameObject particleSystem;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            particleSystem.SetActive(!particleSystem.activeSelf);
+        }
+    }
+}
